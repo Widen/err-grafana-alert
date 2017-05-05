1d285e5d22633cdcc32f6cfae264134348b0667a
Martin Peters <martin@freakybytes.net>
2017-05-05 15:00:39 +0200

    finished foundation, basic commands and webhook

--- err-grafana-alert.py
+++ err-grafana-alert.py
@@ -1,3 +1,7 @@
+import random
+import bottle
+
+
 from errbot import BotPlugin, botcmd, arg_botcmd, webhook
 
 
@@ -5,6 +9,7 @@ class ErrGrafanaAlert(BotPlugin):
     """
     Accepts Grafana webhook calls and posts alert messages in the chat
     """
+    _TOKEN_ALPHABET = 'abcdefghijklmnopqrstuvwxyz0123456789'
 
     def activate(self):
         """
@@ -13,6 +18,10 @@ class ErrGrafanaAlert(BotPlugin):
         You should delete it if you're not using it to override any default behaviour
         """
         super(ErrGrafanaAlert, self).activate()
+
+        if 'INSTANCES' not in self:
+            self.log.warning("Created 'INSTANCES' persistence key")
+            self['INSTANCES'] = {}
 
     def deactivate(self):
         """
@@ -28,9 +37,9 @@ class ErrGrafanaAlert(BotPlugin):
 
         You should delete it if your plugin doesn't use any configuration like this
         """
-        return {'EXAMPLE_KEY_1': "Example value",
-                'EXAMPLE_KEY_2': ["Example", "Value"]
-               }
+        return {
+                'TOKEN_LENGTH': 48,
+                }
 
     def check_configuration(self, configuration):
         """
@@ -42,55 +51,83 @@ class ErrGrafanaAlert(BotPlugin):
         """
         super(ErrGrafanaAlert, self).check_configuration(configuration)
 
-    def callback_connect(self):
-        """
-        Triggers when bot is connected
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        pass
-
-    def callback_message(self, message):
-        """
-        Triggered for every received message that isn't coming from the bot itself
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        pass
-
-    def callback_botmessage(self, message):
-        """
-        Triggered for every message that comes from the bot itself
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        pass
-
     @webhook
     def example_webhook(self, incoming_request):
         """A webhook which simply returns 'Example'"""
         return "Example"
 
-    # Passing split_args_with=None will cause arguments to be split on any kind
-    # of whitespace, just like Python's split() does
-    @botcmd(split_args_with=None)
-    def example(self, message, args):
-        """A command which simply returns 'Example'"""
-        return "Example"
+    @webhook('/grafana/<token>/alert', raw=True)
+    def alert_webhook(self, request, token):
 
-    @arg_botcmd('name', type=str)
-    @arg_botcmd('--favorite-number', type=int, unpack_args=False)
-    def hello(self, message, args):
-        """
-        A command which says hello to someone.
+        try:
+            self.log.info("Try to find Grafana instance with token {token}".format(token=token))
+            instance = self._find_instance_by_token(token)
+            self.log.info("Found Grafana instance {name} via {token}".format(name=instance['name'], token=token))
+        except KeyError:
+            self.log.exception()
+            bottle.abort(403, "Forbidden")
 
-        If you include --favorite-number, it will also tell you their
-        favorite number.
-        """
-        if args.favorite_number is None:
-            return "Hello {name}".format(name=args.name)
-        else:
-            return "Hello {name}, I hear your favorite number is {number}".format(
-                name=args.name,
-                number=args.favorite_number,
-            )
+        self.send(
+                self.build_identifier(instance['room']),
+                'bla bla alerting',
+                )
+
+        return 'OK'
+
+    @arg_botcmd('name', type=str, help='name of the Grafana instance')
+    @arg_botcmd('--url', type=str, default=None, help='Optional URL to the Grafana instance. Used for additional security check')
+    @arg_botcmd('--room', type=str, default=None, help='Defines the room in which the alerts should be posted. Defaults to the current room')
+    @arg_botcmd('--show-images', type=bool, default=True)
+    def grafana_add(self, mess, name, url=None, room=None, show_images=True):
+
+        if not name:
+            return "You need to set at leat a name..."
+        elif name in self['INSTANCES']:
+            # instance already exists
+            return "{name} already exists as Grafana instance.".format(name=name)
+
+        instance = {
+                'name': name,
+                'token': self._generate_token(),
+                'url': url,
+                'show_images': True if show_images is True else False,
+                'room': room if room else str(mess.to),
+                }
+
+        with self.mutable('INSTANCES') as instances:
+            instances[name] = instance
+
+        # send registration notification to target room
+        self.send(
+                self.build_identifier(instance['room']),
+                "Registered Grafana instance {name} for {room}".format(name=name, room=instance['room']),
+                )
+
+        yield "Successfully registered Grafana instance"
+        yield "Please config Grafana to call following webhook: {server}/grafana/{token}/alert".format(server='', token=instance['token'])
+
+    @botcmd
+    def grafana_list(self, mess, args):
+        yield "{count} Grafana instances found".format(count=len(self['INSTANCES']))
+
+        for name, instance in self['INSTANCES'].items():
+            yield "{name} in {room} -> {token}".format(**instance)
+
+    def _generate_token(self, length=None):
+        if not length:
+            length = self.config.get('TOKEN_LENGTH', 128)
+
+        rand = random.SystemRandom()
+        token = []
+        for i in range(0, length):
+            token.append(self._TOKEN_ALPHABET[rand.randint(0, len(self._TOKEN_ALPHABET)-1)])
+
+        return ''.join(token)
+
+    def _find_instance_by_token(self, token):
+
+        for name, instance in self['INSTANCES'].items():
+            if instance['token'] == token:
+                return instance
+
+        raise KeyError("No Grafana instance found with this token {token}".format(token=token))
